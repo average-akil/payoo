ff083a096d229bab67a904e97fc27d77855b2acf
average-akil <ahsoykat22@gmail.com>
2025-05-28 06:37:54 +0600

    step-2

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,13 @@
-console.log("home")
+// add money to the account
+//  step 1 : add addEventListener
+// step 2 : get money to be added to the account balance
+
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+    // s-2 get money to be added in the account
+   const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput)
+
+    const pinNumber = document.getElementById('input-pin-number').value
+    console.log(pinNumber)
+})
